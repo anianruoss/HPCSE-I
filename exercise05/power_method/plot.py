de38249ebf2fa0601562c09e679729e7db8b7702
Anian Ruoss <anian.ruoss@gmail.com>
2018-10-31 23:47:31 +0100

    Plot run times and speedups

--- exercise05/power_method/plot.py
+++ exercise05/power_method/plot.py
@@ -11,18 +11,29 @@ for idx, line in enumerate(content):
         run_times.append(np.fromstring(content[idx + 1], sep=',')[:-1])
 
 alphas = np.array([1. / 8, 1. / 4, 1. / 2, 1, 3. / 2, 2, 4, 8, 16])
+
 run_times_no_blas = run_times[0]
 run_times_blas = run_times[1]
+run_times_lapack = run_times[2]
 
-plt.plot(alphas, run_times_no_blas, 'r--', label='no BLAS')
-plt.plot(alphas, run_times_blas, 'b--', label='BLAS')
+plt.plot(alphas, run_times_no_blas, 'r+:', label='no BLAS')
+plt.plot(alphas, run_times_blas, 'b+:', label='BLAS')
+plt.plot(alphas, run_times_lapack, 'g+:', label='LAPACK')
+plt.xlabel('alphas')
+plt.ylabel('run time [s]')
 plt.legend()
 
 plt.savefig('run_times.png', bbox_inches='tight')
 plt.close()
 
-speedup = np.divide(run_times_no_blas, run_times_blas)
+speedup_blas = np.divide(run_times_no_blas, run_times_blas)
+speedup_lapack = np.divide(run_times_blas, run_times_lapack)
 
-plt.plot(alphas, speedup)
+plt.plot(alphas, speedup_blas, 'r+:', label='$t_{manual}/t_{power}$')
+plt.plot(alphas, speedup_lapack, 'b+:', label='$t_{power}/t_{ev\_full}$')
+plt.xlabel('alphas')
+plt.ylabel('speedup')
+plt.legend()
+
 plt.savefig('speedup.png', bbox_inches='tight')
 plt.close()
