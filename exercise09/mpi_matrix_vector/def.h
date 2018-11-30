13dbd30130796938a53254a2eabf9cb3bbcf9bb5
Anian Ruoss <anian.ruoss@gmail.com>
2018-11-30 16:44:21 +0100

    Refactor

--- exercise09/mpi_matrix_vector/def.h
+++ exercise09/mpi_matrix_vector/def.h
@@ -22,8 +22,8 @@ const Size BY = NY / NBY;
 // number of points per rank
 const Size L = BX * BY;
 
-static_assert(NX % NBX == 0 && NY % NBY == 0, 
-    "Grid size not divisible by number of blocks");
+static_assert(NX % NBX == 0 && NY % NBY == 0,
+              "Grid size not divisible by number of blocks");
 
 using VR = std::vector<Real>;
 using VI = std::vector<Size>;
@@ -31,13 +31,12 @@ using VI = std::vector<Size>;
 // Distributed matrix.
 // i: local row, gj: global column, k: index in a
 struct Matr {
-  VR a;         // data, size nnz, a[k], k=[0..nnz)
-  VI ki = {0};  // index in a, size n+1, k[i], i=[0..n]
-  VI gjk;       // global column, size nnz, j[k], k=[0..nnz)
-  Size n = 0; // number of equations
+  VR a;        // data, size nnz, a[k], k=[0..nnz)
+  VI ki = {0}; // index in a, size n+1, k[i], i=[0..n]
+  VI gjk;      // global column, size nnz, j[k], k=[0..nnz)
+  Size n = 0;  // number of equations
 };
 
 struct Vect {
   VR v;
 };
-
