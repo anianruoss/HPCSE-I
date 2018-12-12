8a1a31b7584b7c84abc1fd8ee8ade7756e9a6ae3
Anian Ruoss <anian.ruoss@gmail.com>
2018-12-12 23:55:25 +0100

    Refactor

--- exercise11/diffusion_adi/timer.hpp
+++ exercise11/diffusion_adi/timer.hpp
@@ -14,9 +14,9 @@ public:
     stop_time.tv_usec = 0;
   }
 
-  inline void start() { gettimeofday(&start_time, NULL); }
+  inline void start() { gettimeofday(&start_time, nullptr); }
 
-  inline void stop() { gettimeofday(&stop_time, NULL); }
+  inline void stop() { gettimeofday(&stop_time, nullptr); }
 
   double get_timing() const {
     return (stop_time.tv_sec - start_time.tv_sec) +
