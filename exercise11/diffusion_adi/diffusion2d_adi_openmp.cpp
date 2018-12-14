0f99c3aa537df530831f330350ace91c645c5339
Anian Ruoss <anian.ruoss@gmail.com>
2018-12-14 01:19:32 +0100

    Implement ADI scheme for diffusion equation

--- exercise11/diffusion_adi/diffusion2d_adi_openmp.cpp
+++ exercise11/diffusion_adi/diffusion2d_adi_openmp.cpp
@@ -17,7 +17,7 @@ struct Diagnostics {
 
 class Diffusion2D {
 public:
-  Diffusion2D(const double D, const double L, const int N, const double dt,
+  Diffusion2D(const double D, const double L, const size_t N, const double dt,
               const int rank)
       : D_(D), L_(L), N_(N), dt_(dt), rank_(rank) {
     // Real space grid spacing.
@@ -36,32 +36,87 @@ public:
     initialize_rho();
 
     R_ = 2. * dr_ * dr_ / dt_;
-    // TODO:
-    // Initialize diagonals of the coefficient
-    // matrix A, where Ax=b is the corresponding
-    // system to be solved
-    // ...
+
+    // Initialize diagonals of the coefficient matrix A, where Ax=b is the
+    // corresponding system to be solved
+    const double a = -D / R_;
+    const double b = 1 + 2. * D / R_;
+    const double c = -D / R_;
+
+    a_.resize(real_N_, a);
+    b_.resize(real_N_, b);
+    c_.resize(real_N_, c);
+
+    a_[0] = a_[1] = a_[real_N_ - 1] = 0.;
+    c_[0] = c_[real_N_ - 2] = c_[real_N_ - 1] = 0.;
+    b_[0] = b_[real_N_ - 1] = 1.;
+
+    // pre-compute first step of Thomas algorithm
+    for (size_t i = 1; i < real_N_; ++i) {
+      b_[i] -= c_[i - 1] * a_[i] / b_[i - 1];
+      assert(b_[i] != 0);
+    }
   }
 
   void advance() {
-    // TODO:
-    // Implement the ADI scheme for diffusion
-    // and parallelize with OpenMP
-    // ...
-
-    // ADI Step 1: Update rows at half timestep
+    // ADI Step 1: Update rows at half time step
     // Solve implicit system with Thomas algorithm
-    // ...
+    std::vector<double> v(real_N_, 0.);
 
-    // ADI: Step 2: Update columns at full timestep
+    for (size_t j = 1; j < real_N_ - 1; ++j) {
+      v[0] = v[real_N_ - 1] = 0.;
+
+      for (size_t i = 1; i < real_N_ - 1; ++i) {
+        v[i] =
+            D_ * (rho_[i * real_N_ + j + 1] + rho_[i * real_N_ + j - 1]) / R_ +
+            (1. - 2. * D_ / R_) * rho_[i * real_N_ + j];
+      }
+
+      // first step of Thomas algorithm
+      for (size_t i = 1; i < real_N_; ++i) {
+        v[i] -= v[i - 1] * a_[i] / b_[i - 1];
+      }
+
+      // second step of Thomas algorithm
+      rho_[(real_N_ - 1) * real_N_ + j] = v[real_N_ - 1] / b_[real_N_ - 1];
+
+      for (int i = static_cast<int>(real_N_) - 2; i >= 0; --i) {
+        rho_[i * real_N_ + j] =
+            (v[i] - c_[i] * rho_[(i + 1) * real_N_ + j]) / b_[i];
+      }
+    }
+
+    // ADI Step 2: Update columns at full time step
     // Solve implicit system with Thomas algorithm
-    // ...
+    for (size_t i = 1; i < real_N_ - 1; ++i) {
+      v[0] = v[real_N_ - 1] = 0.;
+
+      for (size_t j = 1; j < real_N_ - 1; ++j) {
+        v[j] = D_ *
+                   (rho_[(i + 1) * real_N_ + j] + rho_[(i - 1) * real_N_ + j]) /
+                   R_ +
+               (1. - 2. * D_ / R_) * rho_[i * real_N_ + j];
+      }
+
+      // first step of Thomas algorithm
+      for (size_t j = 1; j < real_N_; ++j) {
+        v[j] -= v[j - 1] * a_[j] / b_[j - 1];
+      }
+
+      // second step of Thomas algorithm
+      rho_[i * real_N_ + (real_N_ - 1)] = v[real_N_ - 1] / b_[real_N_ - 1];
+
+      for (int j = static_cast<int>(real_N_) - 2; j >= 0; --j) {
+        rho_[i * real_N_ + j] =
+            (v[j] - c_[j] * rho_[i * real_N_ + (j + 1)]) / b_[j];
+      }
+    }
   }
 
   void compute_diagnostics(const double t) {
     double heat = 0.0;
-    for (int i = 1; i <= N_; ++i)
-      for (int j = 1; j <= N_; ++j)
+    for (size_t i = 1; i <= N_; ++i)
+      for (size_t j = 1; j <= N_; ++j)
         heat += dr_ * dr_ * rho_[i * real_N_ + j];
 
 #if DEBUG
@@ -78,27 +133,22 @@ public:
   }
 
 private:
+  /* Initialize rho(x, y, t=0) */
   void initialize_rho() {
-    /* Initialize rho(x, y, t=0) */
-
     double bound = 0.25 * L_;
-
-    // TODO:
-    // Initialize field rho based on the
-    // prescribed initial conditions
-    // and parallelize with OpenMP
-    // ...
-
-    for (int i = 1; i <= N_; ++i) {
-      for (int j = 1; j <= N_; ++j) {
-
-        // ...
+    // Initialize rho based on the prescribed initial conditions
+    for (size_t i = 1; i <= N_; ++i) {
+      for (size_t j = 1; j <= N_; ++j) {
+        if ((std::abs((i - 1.) * dr_ - L_ / 2.) < bound) &&
+            (std::abs((j - 1.) * dr_ - L_ / 2.) < bound)) {
+          rho_[i * real_N_ + j] = rho_tmp_[i * real_N_ + j] = 1.;
+        }
       }
     }
   }
 
   double D_, L_;
-  int N_, Ntot_, real_N_;
+  size_t N_, Ntot_, real_N_;
   double dr_, dt_;
   double R_;
   int rank_;
@@ -121,8 +171,8 @@ int main(int argc, char *argv[]) {
 
   const double D = std::stod(argv[1]);  // diffusion constant
   const double L = std::stod(argv[2]);  // domain side size
-  const int N = std::stoul(argv[3]);    // number of grid points per dimension
-  const double dt = std::stod(argv[4]); // timestep
+  const size_t N = std::stoul(argv[3]); // number of grid points per dimension
+  const double dt = std::stod(argv[4]); // time step
 
   Diffusion2D system(D, L, N, dt, 0);
 
